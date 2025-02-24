8c54e9d73168af9bbd3271e2084165801d1c53f5
Nihad İbadzade <114952145+Nihadname@users.noreply.github.com>
2025-02-24 19:02:56 +0400

    done

--- Core/EbuBridgeLmsSystem.Application/BackgroundServices/UserPermanentDeleteBackgroundService.cs
+++ Core/EbuBridgeLmsSystem.Application/BackgroundServices/UserPermanentDeleteBackgroundService.cs
@@ -1,7 +1,9 @@
 ﻿using EbuBridgeLmsSystem.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
 
 namespace EbuBridgeLmsSystem.Application.BackgroundServices
 {
@@ -29,7 +31,22 @@ namespace EbuBridgeLmsSystem.Application.BackgroundServices
             throw new NotImplementedException();
         }
         private async Task DeleteAllSoftDeletedUsers() { 
-           
+           var allSoftDeletedUsers=await _userManager.Users.Where(s=>s.IsDeleted==true&&s.DeletedTime!=null).ToListAsync();
+            var span = CollectionsMarshal.AsSpan(allSoftDeletedUsers);
+            for (int i = 0; i < allSoftDeletedUsers.Count; i++)
+            {
+                var deletedTime = span[i].DeletedTime;
+                var diffrenceBetweenDeletedTimeAndNow = DateTime.UtcNow.Subtract((DateTime)deletedTime).Days;
+                if (diffrenceBetweenDeletedTimeAndNow >= 7)
+                {
+                  var result=  await _userManager.DeleteAsync(span[i]);
+                    if (!result.Succeeded)
+                    {
+                        _logger.LogError(result.ToString(), "Error occurred while creating fees.");
+                    }
+                   
+                }
+            }
         }
     }
 }
