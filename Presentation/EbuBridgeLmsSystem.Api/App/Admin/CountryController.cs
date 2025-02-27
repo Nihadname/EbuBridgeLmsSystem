55f59482e97457f4eade9811ea09830722dcffd8
Nihad İbadzade <114952145+Nihadname@users.noreply.github.com>
2025-02-27 17:34:00 +0400

    done

--- Presentation/EbuBridgeLmsSystem.Api/App/Admin/CountryController.cs
+++ Presentation/EbuBridgeLmsSystem.Api/App/Admin/CountryController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using EbuBridgeLmsSystem.Api.Extensions;
+using EbuBridgeLmsSystem.Application.Dtos.Country;
+using EbuBridgeLmsSystem.Application.Features.CountryFeature.Commands.CreateCountry;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EbuBridgeLmsSystem.Api.App.Admin
 {
-    [Route("api/[controller]")]
+    [Area("Admin")]
+    [Route("api/[area]/[controller]")]
     [ApiController]
     public class CountryController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCountry(CountryCreateDto countryCreateDto)
+        {
+            var mappedCommand=_mapper.Map<CreateCountryCommand>(countryCreateDto);
+            var result=await _mediator.Send(mappedCommand);
+            return this.ToActionResult(result);
+        }
     }
 }
