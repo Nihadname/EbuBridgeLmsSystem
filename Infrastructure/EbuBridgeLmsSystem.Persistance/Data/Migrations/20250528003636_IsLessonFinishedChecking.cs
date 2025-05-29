bc6234d4c610d1a236850cba47ece99d4ba943d2
nihadibadzade <nihadcoding@gmail.com>
2025-05-30 01:15:00 +0400

    commit unproccesed files

---
+++ Infrastructure/EbuBridgeLmsSystem.Persistance/Data/Migrations/20250528003636_IsLessonFinishedChecking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EbuBridgeLmsSystem.Persistance.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsLessonFinishedChecking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isLessonFinished",
+                table: "lessonUnitAssignments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isLessonFinished",
+                table: "lessonUnitAssignments");
+        }
+    }
+}
